e0b2483f10db10d185a10207f36c239d54404ee1
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-04 20:57:11 +0300

    New Project

---
+++ car-marketplace/src/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { SignIn, SignInButton } from '@clerk/clerk-react'
+
+function Home() {
+  return (
+    <div>
+          {/* Header */}
+
+          {/* Hero */}
+    </div>
+  )
+}
+
+export default Home
---
+++ car-marketplace/vite.config.js
@@ -0,0 +1,50 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// Get the directory name
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
