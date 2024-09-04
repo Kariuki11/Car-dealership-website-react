6428775d9add077aa3944b04d03839127bb56a52
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-05 00:25:37 +0300

    New changes

---
+++ car-marketplace/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { UserButton, useUser } from '@clerk/clerk-react'
+import React from 'react'
+import { Button } from './ui/button';
+
+function Header() {
+    const {user, isSignedIn}=useUser();
+  return (
+    <div >
+        <img src='/logo.svg' width={150} height={100} alt="logo" />
+
+        <ul>
+            <li>Home</li>
+            <li>Search</li>
+            <li>New</li>
+            <li>Preowned</li>
+        </ul>
+
+        {isSignedIn?
+            <div>
+                <UserButton/>
+                <Button>Submit Listing</Button>
+            </div>
+            :
+            <Button>Sbmit Listing</Button>
+        }
+
+    </div>
+  )
+}
+
+export default Header
--- car-marketplace/src/home.jsx
+++ car-marketplace/src/home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button } from './components/ui/button'
-import { SignIn, SignInButton } from '@clerk/clerk-react'
+import { SignInButton } from '@clerk/clerk-react'
+import Header from './components/Header'
 
 function Home() {
   return (
     <div>
           {/* Header */}
-
+          <Header />
           {/* Hero */}
     </div>
   )
