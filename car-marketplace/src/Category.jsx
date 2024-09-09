22612b75f785b106f54f8b5c833572bb21d6ed5a
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-09 14:14:39 +0300

    Changes

--- car-marketplace/src/Category.jsx
+++ car-marketplace/src/Category.jsx
@@ -6,8 +6,8 @@ function Category() {
     <div className='mt-40'>
         <h2 className='font-bold text-3xl text-center mb-6'>Browse By Type</h2>
 
-        <div>
-
+        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6
+        lg:grid-cols-9 gap-6 px-20'>
             {Data.Category.map((category, index) => (
             <div key={category.id || index}> {/* Added key prop */}
                 <img src={category.icon} width={40} height={40} />
