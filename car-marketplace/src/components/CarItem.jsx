82cfd5547dc6899cdb1083f7e08ce385b51760ef
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-09 14:54:32 +0300

    New changes

---
+++ car-marketplace/src/components/CarItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CarItem({car}) {
+  return (
+    <div>CarItem</div>
+  )
+}
+
+export default CarItem
--- car-marketplace/src/components/MostSearchedCar.jsx
+++ car-marketplace/src/components/MostSearchedCar.jsx
@@ -1,10 +1,17 @@
 import FakeData from '@/Shared/FakeData'
 import React from 'react'
+import CarItem from './CarItem';
 
 function MostSearchedCar() {
   console.log(FakeData.carList);
   return (
-    <div>MostSearchedCar</div>
+    <div>
+      <h2 className='font-bold text-3xl text-center my-16'>Most Searched Cars</h2>
+
+      {FakeData.carList.map(({car,index})=>(
+        <CarItem car={car} key={index}/>
+      ))}
+    </div>
   )
 }
 
