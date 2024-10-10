954339bcc522b81ac950d6a4b5f00efb9d120ce0
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-10 17:40:48 +0300

    "Removed logo.svg and react.svg files, deleted tesla.png, and updated Header.jsx component to use an external image URL instead of logo.svg"

--- car-marketplace/src/components/Header.jsx
+++ car-marketplace/src/components/Header.jsx
@@ -1,31 +1,34 @@
 import { UserButton, useUser } from '@clerk/clerk-react'
 import React from 'react'
 import { Button } from './ui/button';
+import { logo, } from '../assets';
+
 
 function Header() {
-    const {user, isSignedIn}=useUser();
-  return (
-    <div className='flex justify-between items-center shadow-sm p-5'>
-        <img src='/logo.svg' width={150} height={100} alt="logo" />
+    const { user, isSignedIn } = useUser();
+    return (
+        <div className='flex justify-between items-center shadow-sm p-5'>
+            <img src='https://i.pinimg.com/736x/e1/10/03/e11003d6d3ea43ef832e52a112ab7bac.jpg' width={150} height={100} alt="logo" />
+            {/* <img src={logo} width={150} height={100} alt="logo" /> */}
 
-        <ul className='hidden md:flex gap-16'>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>New</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Preowned</li>
-        </ul>
+            <ul className='hidden md:flex gap-16'>
+                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
+                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
+                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>New</li>
+                <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Preowned</li>
+            </ul>
 
-        {isSignedIn?
-            <div className='flex items-center gap-5'>
-                <UserButton/>
-                <Button>Submit Listing</Button>
-            </div>
-            :
-            <Button>Sbmit Listing</Button>
-        }
+            {isSignedIn ?
+                <div className='flex items-center gap-5'>
+                    <UserButton />
+                    <Button>Submit Listing</Button>
+                </div>
+                :
+                <Button>Sbmit Listing</Button>
+            }
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Header
