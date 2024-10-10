0ed2d370675bbbfc9de5e910a9e78aa23b3076a7
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-10 17:41:43 +0300

    Restore logo image in Header component and update image source

--- car-marketplace/src/components/Header.jsx
+++ car-marketplace/src/components/Header.jsx
@@ -8,8 +8,8 @@ function Header() {
     const { user, isSignedIn } = useUser();
     return (
         <div className='flex justify-between items-center shadow-sm p-5'>
-            <img src='https://i.pinimg.com/736x/e1/10/03/e11003d6d3ea43ef832e52a112ab7bac.jpg' width={150} height={100} alt="logo" />
-            {/* <img src={logo} width={150} height={100} alt="logo" /> */}
+            {/* <img src='https://i.pinimg.com/736x/e1/10/03/e11003d6d3ea43ef832e52a112ab7bac.jpg' width={150} height={100} alt="logo" /> */}
+            <img src={logo} width={150} height={100} alt="logo" />
 
             <ul className='hidden md:flex gap-16'>
                 <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
